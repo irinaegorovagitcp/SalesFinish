031bd764b1c182eeb24d1c528dca1b1e06b0a801
EgorovaIrina <Irina1978@yandex.ru>
2022-11-28 15:32:17 +0300

    One

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[] {4, 6, 9});
+        System.out.println(salesManager.max());
+    }
+}
